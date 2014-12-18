4f9dc6a8abc2dc0c8459f7b162fab2e63809bdba
Harry Chen <harrychenca@gmail.com>
2014-12-19 00:37:24 +0800

    Animations over chart is working

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,13 +1,24 @@
 function scrollToBottomOfMessages(){
-  $('#messages_box').animate({ scrollTop: $('#messages_box').prop("scrollHeight") }, 'slow');
+  $('#messages_box').animate({ scrollTop: $('#messages_box').prop('scrollHeight') }, 'slow');
 }
 
 function screenTextAnimation(content){
-  console.log(content);
-  $('#screen_text').text(content);
-  
-  $('#screen_text').show().css({'left': '0px', 'top': '0px'});
-  $('#screen_text').animate({left:'30%', top:'30%'}).fadeOut();
+  var element = '<div class="screen_text not_done">'+content+'</div>'
+
+  $('#screen_texts').append($(element));
+
+  $('.screen_text.not_done').show().css({'left': '0px', 'top': '0px'});
+
+  $('.screen_text.not_done').animate({
+      left:'30%', top:'30%'
+    },
+    {
+      duration: 5000,
+      complete: function(){
+        $('.screen_text').removeClass("not_done")
+      }
+    }
+  ).fadeOut('slow');
 }
 
 $(document).ready(function(){
