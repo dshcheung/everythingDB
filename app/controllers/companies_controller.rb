8599eb3ec1ccc06586d5a823dc9e8bbbf5935172
Harry Chen <harrychenca@gmail.com>
2014-12-26 20:53:49 +0800

    Added indicators for high gain dates

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,5 +9,34 @@ class CompaniesController < ApplicationController
     gon.company = @company
 
     # TODO: biggest 3 historical spread (datetime, spread, volume)
+    matching_quotes = []
+    @connected_quote_arr = []
+    temp_quote_arr = []
+
+    @company.daily_quotes.each do |quote|
+      if (quote.close - quote.open) / quote.open >= 0.05
+        matching_quotes << quote
+      end
+    end
+
+    matching_quotes.each_with_index do |date, index|
+      if index == matching_quotes.length - 1
+        return
+      end
+
+      if matching_quotes[index].date + 1.day == matching_quotes[index+1].date
+        if temp_quote_arr == []
+          temp_quote_arr << matching_quotes[index]
+          temp_quote_arr << matching_quotes[index+1]
+        else
+          temp_quote_arr << matching_quotes[index+1]
+        end
+      else
+        @connected_quote_arr << temp_quote_arr if temp_quote_arr.any?
+        temp_quote_arr = []
+      end
+    end
+
+    @connected_quote_arr
   end
 end
