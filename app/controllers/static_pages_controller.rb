92872d496822b9696c7fe8c7c5a6d3e750db7a1b
Harry Chen <harrychenca@gmail.com>
2014-12-30 11:35:51 +0800

    Added daily price rise from 5-10%

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,43 @@ class StaticPagesController < ApplicationController
     
   end
 
-  def test
-  	
+  def daily_five_percent 
+    @five_or_more_percent = []
+    @six_or_more_percent = []
+    @seven_or_more_percent = []
+    @eight_or_more_percent = []
+    @nine_or_more_percent = []
+    @ten_or_more_percent = []
+
+    one_day_ago = DailyQuote.order(:date).last.date
+
+    @daily_quotes = DailyQuote.where("date = ?", one_day_ago)
+
+    @daily_quotes.each do |q|
+      if check_gain(q, 0.1)
+        @ten_or_more_percent << q
+      elsif check_gain(q, 0.09)
+        @nine_or_more_percent << q
+      elsif check_gain(q, 0.08)
+        @eight_or_more_percent << q
+      elsif check_gain(q, 0.07)
+        @seven_or_more_percent << q
+      elsif check_gain(q, 0.06)
+        @six_or_more_percent << q
+      elsif check_gain(q, 0.05)
+        @five_or_more_percent << q
+      end
+    end
   end
+
+  def two_day_five_percent 
+    two_days_ago = Company.first.daily_quotes.order(:date).last(2).first.date
+  end
+
+  protected
+
+    def check_gain(quote, gain)
+      # need to ensure volume is greater than 0 on that day
+      return (quote.close - quote.open) / quote.open >= gain && quote.volume > 0
+    end
 end
--- app/models/daily_quote.rb
+++ app/models/daily_quote.rb
@@ -2,4 +2,12 @@ class DailyQuote < ActiveRecord::Base
   belongs_to :company
 
   validates_uniqueness_of :date, :scope => :company_id
+
+  # scope :last_two_days, -> { where("date > ?", :two_days_ago) }
+
+  # private
+
+  #   def two_days_ago
+  #     2.days.ago
+  #   end
 end
