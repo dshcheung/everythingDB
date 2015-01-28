df56adf267749fe2269a02ccad8ccfa81f04648b
Paul <paul.lugagnedelpon@gmail.com>
2015-01-29 00:52:06 +0800

    feat(FlightRadar: Finalise the templates, add back buttons)

--- app/assets/javascripts/angular/controllers/airlines.js
+++ app/assets/javascripts/angular/controllers/airlines.js
@@ -4,19 +4,22 @@ app.controller('AirlineListCtrl', ['$scope', '$http', function($scope, $http){
   });
 }]);
 
-
 app.controller('AirlineItemCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
   $http.get('api/v1/airlines/' + $routeParams.icao + '.json').success(function(data) {
     $scope.airline = data.airlines[0];
   });
-  $http.get('api/v1/airplanes/' + $routeParams.icao + '.json').success(function(data) {
+}]);
+
+
+app.controller('AirplaneListCtrl', ['$scope', '$http', '$routeParams', function($scope, $http){
+  console.log('hello from AirplaneListCtrl');
+  $http.get('api/v1/airplanes.json').success(function(data) {
     $scope.airplanes = data.airplanes;
   });
 }]);
 
-
 app.controller('AirplaneItemCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
-  $http.get('api/v1/airplanes/' + $routeParams.airline_icao + '/' + $routeParams.registration_code + '.json').success(function(data) {
+  $http.get('api/v1/airplanes/' + $routeParams.registration_code + '.json').success(function(data) {
     $scope.airplane = data.airplanes[0];
   });
 }]);
